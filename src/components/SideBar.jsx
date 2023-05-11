d51810495bbb81c5412128ed18f4f005c381d7bc
Alex Kitsu <9apsilva@gmail.com>
2023-05-10 21:19:59 -0300

    feat: added side bar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,34 @@
+import { Stack } from "@mui/material"
+import { categories } from "../utils/constants"
+import { useState } from "react"
+
+export const SideBar = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  
+  function handleClickCategory(name) {
+    setSelectedCategory(name)
+  }
+
+  return (
+    <Stack direction='row' sx={{ overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: "column" } }}>
+      {categories.map(category => (
+        <button 
+        key={category.name} 
+        className='category-btn' 
+        style={{
+          background: category.name === selectedCategory && '#FC1503',
+          color: '#FFF'
+        }}
+        onClick={() => handleClickCategory(category.name)}
+        >
+          <span style={{ color: category.name === selectedCategory ? '#FFF' : '#FC1503', marginRight: '15px' }}>
+            {category.icon}
+          </span>
+          <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>
+            {category.name}
+          </span>
+        </button>
+      ))}
+    </Stack>
+  )
+}
