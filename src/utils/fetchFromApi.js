21b32e9d86fdcb84aacf3a86cd05442cdccedbd8
Alex Kitsu <9apsilva@gmail.com>
2023-05-14 19:57:49 -0300

    feat: added rapid api base config

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const baseUrl = 'https://youtube-v31.p.rapidapi.com'
+
+const options = {
+  params: {
+    regionCode: 'BR',
+    maxResults: '10',
+    order: 'date'
+  },
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+}
+
+export const FetchApi = async (endpoint) => {
+  const { data } = await axios.get(`${baseUrl}/${endpoint}`,options)
+  return data
+}
