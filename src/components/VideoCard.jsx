8107885267b1578e9938c0992ac46be7a0ad6ba4
Alex Kitsu <9apsilva@gmail.com>
2023-05-15 19:41:11 -0300

    feat: added descriptions in video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,20 +1,35 @@
-import { Card, CardMedia } from '@mui/material'
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
 import { Link } from 'react-router-dom'
+import { CheckCircle } from '@mui/icons-material'
 
 export const VideoCard = ({ video }) => {
   const { videoId } = video.id
   const { snippet } = video
   console.log(snippet)
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 'none' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet.thumbnails.high.url}
           alt={snippet.title}
-          sx={{ height: 358, width: 180 }}
+          sx={{ width: 358, height: 180 }}
         />
       </Link>
+      <CardContent sx={{backgroundColor: '#1e1e1e', height: '106px'}}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' color='#FFF' fontWeight='bold'>
+            {snippet.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        
+        <Link to={snippet.channelId ? `/channel/${snippet.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' color='gray' fontWeight='bold'>
+            {snippet.demoChannelTitle || demoChannelTitle}
+            <CheckCircle sx={{fontSize: 14, color: 'gray', ml: '6px'}}/>
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,12 +4,12 @@ import { VideoCard, ChannelCard } from "./"
 
 export const Videos = ({ videos }) => {
   return (
-    <Stack flexDirection={'row'} flexWrap={'wrap'} justifyContent={'start'} gap={2}>
+    <Stack flexDirection='row' flexWrap='wrap' justifyContent='start' gap={2}>
       {
         videos.map((video, i) => (
           <Box key={i}>
             {video.id?.videoId && <VideoCard video={video} />}
-            {video.id?.ChannelId && <ChannelCard channelDetails={video} />}
+            {video.id?.channelId && <ChannelCard channelDetails={video} />}
           </Box>
         ))
       }
