637f1071d6c7bbf66e7d0053b6c8a1113c65dd32
Alex Kitsu <9apsilva@gmail.com>
2023-05-16 22:09:19 -0300

    feat: added channel detail component

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,11 +1,11 @@
 import { Box, CardContent, CardMedia, Typography } from '@mui/material'
-import { demonProfileContent } from '../utils/constants'
+import { demoChannelUrl } from '../utils/constants'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { CheckCircle } from '@mui/icons-material'
 
-export const ChannelCard = ({ channelDetails }) => {
-  console.log(channelDetails)
+export const ChannelCard = ({ channelDetails, marginTop }) => {
+  
   return (
     <Box sx={{
       boxShadow: 'none',
@@ -15,24 +15,25 @@ export const ChannelCard = ({ channelDetails }) => {
       alignItems: 'center',
       width: { md: '320px', xs: '356px' },
       height: '326px',
-      margin: 'auto'
+      margin: 'auto',
+      marginTop: marginTop
     }}>
-      <Link to={`/channel/${channelDetails.id.channelId}`}>
+      <Link to={`/channel/${channelDetails.id}`}>
         <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#FFF' }}>
           <CardMedia
-            image={channelDetails.snippet.thumbnails.high.url || demonProfileContent}
-            alt={channelDetails.snippet.title}
+            image={channelDetails.snippet?.thumbnails.high.url || demoChannelUrl}
+            alt={channelDetails.snippet?.title}
             sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
           />
 
           <Typography variant='h6'>
-            {channelDetails.snippet.title}
+            {channelDetails.snippet?.title}
             <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '6px' }} />
           </Typography>
 
-          {channelDetails.statistics.subscriberCount && (
+          {channelDetails.statistics?.subscriberCount && (
             <Typography>
-              {parseInt(channelDetails.statistics.subscriberCount).toLocaleString()} Inscritos
+              {parseInt(channelDetails.statistics?.subscriberCount).toLocaleString()} Inscritos
             </Typography>
           )}
         </CardContent>
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { FetchApi } from "../utils/fetchFromApi"
+import { Box } from "@mui/material"
+import { ChannelCard } from "./"
+import { Videos } from './'
+
 export const ChannelDetail = () => {
+  const { id } = useParams()
+  const [details, setDetails] = useState([])
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const details = await FetchApi(`channels?part=snippet%2Cstatistics&id=${id}`)
+      setDetails(details.items[0])
+
+      const videos = await FetchApi(`search?channelId=${id}&part=snippet&order=date`)
+      setVideos(videos.items)
+
+    })()
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(0, 238, 247, 1) 0%, rgba(206, 3, 184, 1) 100%, rgba(0, 212, 255, 1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }}
+        />
+        {console.log(details)}
+        <ChannelCard channelDetails={details} marginTop='-110px' />
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
